8cf56a3f6d32fdbf75abc7bf2cdbad397a2ac3ae
Manne Srija <142595718+Srija-Manne@users.noreply.github.com>
2024-06-02 23:39:27 +0530

    Create Homepage.js

---
+++ Homepage.js
@@ -0,0 +1,15 @@
+
+
+import React from 'react';
+
+const HomePage = () =>
+    {
+        return (
+            <div>
+                  <p>
+                      Hi Welocme
+                  </p>
+            </div>
+        );
+    };
+export default HomePage;
